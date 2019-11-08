e0c1b93d6218df5fd56ea3e0ae29b5563397aeda
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2019-11-08 22:35:29 +0700

    add file

--- app.js
+++ app.js
@@ -1,6 +1,27 @@
 const http = require('http');
-const routes = require('./routes');
+const express = require('express');
+const app = express();
 
-const server = http.createServer(routes);
+// Answer for task 2
 
-server.listen(3000);
+app.use((req, res, next) => {
+    console.log('Hello1');
+});
+
+app.use((req, res, next) => {
+    console.log('Hello2');
+    res.send('Hello2');
+});
+
+// Answer for task 3
+
+// app.use('/', (req, res, next) => {
+//     console.log('This is home!');
+//     res.send('<h1>This is Home</h1>');
+// });
+
+// app.use('/users', (req, res, next) => {
+//     console.log('This is users!');
+//     res.send('<h1>This is Users</h1>');
+// });
+app.listen(3000);
