eb054efb596845ad402f9a1eee74090ca8992a6d
Muhammad Satrio Wicaksono <muhsatrio@gmail.com>
2019-11-14 11:37:08 +0700

    added models

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
     res.render('add-product', {pageTitle: 'Add Product', path: '/admin/add-product'});
@@ -6,12 +6,13 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-    // console.log(req.body.title);
-    products.push({title: req.body.title});
+    const product = new Product(req.body.title);
+    product.save();
     res.redirect('/');
 };
 
 exports.getProducts =  (req, res, next) => {
+    const products = Product.fetchAll();
     // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-    res.render('shop', {products: products, pageTitle: 'My Shop', path: '/', hasProducts: products.length > 0});
+    res.render('shop', {products: products, pageTitle: 'My Shop', path: '/'});
 };
